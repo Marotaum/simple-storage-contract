d6a5a71dcf0833a6e719ff951692184613e688c4
marotaum <ildomm@hotmail.com>
2018-01-07 20:22:31 -0200

    First commit

---
+++ truffle.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+    "live": {
+      network_id: 1,
+      host: "127.0.0.1",
+      port: 8546   // Different than the default below
+    }
+  },
+  rpc: {
+    host: "127.0.0.1",
+    port: 8545
+  }
+};
